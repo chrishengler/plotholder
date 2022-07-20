cd3eb45f1eb2446f4e91e24f961bd62646150fc9
Chris Hengler <dr.c.hengler@gmail.com>
2022-07-20 18:03:10 +0200

    requirements and formatting

--- plotholder/tests/test_data_producer.py
+++ plotholder/tests/test_data_producer.py
@@ -1,5 +1,6 @@
 from plotholder.src.data_producer import random_float_series, random_integer_series
 
+
 def test_integer_data_series_no_limits():
     random_ints = random_integer_series(100)
     assert len(random_ints) == 100
@@ -7,12 +8,14 @@ def test_integer_data_series_no_limits():
         assert type(i) == int
         assert 0 <= i <= 10000
 
+
 def test_integer_data_series_limits():
-    random_ints = random_integer_series(200,-50,50)
+    random_ints = random_integer_series(200, -50, 50)
     assert len(random_ints) == 200
     for i in random_ints:
         assert type(i) == int
         assert -50 <= i <= 50
+
 
 def test_float_data_series_no_limits():
     random_floats = random_float_series(100)
@@ -21,8 +24,9 @@ def test_float_data_series_no_limits():
         assert type(i) == float
         assert 0 <= i <= 10000
 
+
 def test_float_data_series_limits():
-    random_floats = random_float_series(200,-250,-50)
+    random_floats = random_float_series(200, -250, -50)
     assert len(random_floats) == 200
     for i in random_floats:
         assert type(i) == float
