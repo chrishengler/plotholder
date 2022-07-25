08018c8035ffeaa336170c434420eeb796110538
Chris Hengler <dr.c.hengler@gmail.com>
2022-07-25 20:56:14 +0200

    bar chart axis labels, pie charts

--- plotholder/src/plot_gen.py
+++ plotholder/src/plot_gen.py
@@ -4,9 +4,22 @@ from matplotlib import pyplot as plt
 from PIL import Image
 
 
-def bar_chart(x: list[int], y: list[int | float]):
+def bar_chart(x: list[int], y: list[int | float], x_title: str = None, y_title: str = None) -> Image:
     plt.bar(x, y, width=1)
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
     buf = BytesIO()
     plt.savefig(buf)
+    plt.clf()
+    buf.seek(0)
+    return Image.open(buf).convert("RGB")
+
+def pie_chart(amounts: list[float], labels: list[str] = None, title: str = None) -> Image:
+    segments, text = plt.pie(amounts)
+    plt.legend(segments, labels)
+    plt.title(title)
+    buf = BytesIO()
+    plt.savefig(buf)
+    plt.clf()
     buf.seek(0)
     return Image.open(buf).convert("RGB")
--- plotholder/tests/test_plot_gen.py
+++ plotholder/tests/test_plot_gen.py
@@ -2,7 +2,7 @@ import os
 
 from PIL import Image, ImageChops
 
-from plotholder.src.plot_gen import bar_chart
+from plotholder.src.plot_gen import bar_chart, pie_chart
 
 
 def relative_path_to_full_path(rel_path: str) -> str:
@@ -13,6 +13,15 @@ def relative_path_to_full_path(rel_path: str) -> str:
 def test_bar():
     x = [0, 3, 4, 7]
     y = [3, 1, 0, 8]
-    img = bar_chart(x, y)
+    img = bar_chart(x, y, "x-axis", "y-axis")
     target = Image.open(relative_path_to_full_path("resources/bar_chart.png"))
+    # img.save('./resources/bar_chart.png')
+    assert ImageChops.difference(img, target).getbbox() is None
+
+def test_pie_with_autopct():
+    amounts = [1, 2, 4, 8]
+    labels = ["this", "is", "a", "pie"]
+    img = pie_chart(amounts, labels, title="pie chart title")
+    target = Image.open(relative_path_to_full_path("resources/pie_chart.png"))
+    # img.save('./resources/pie_chart.png')
     assert ImageChops.difference(img, target).getbbox() is None
